4829d12c8ac0569a9352a2c9d453f46c9af1c95d
Lam Nguyễn <33188123+LamPocketVN@users.noreply.github.com>
2019-08-12 16:43:24 +0700

    Fix Main Class not found error

--- PointApiAddon.php
+++ PointApiAddon.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 /**
  * @name PointApiAddon
  * @version 1.0.0
- * @main JackMD\ScoreHud\Addons\PointApiApiAddon
+ * @main JackMD\ScoreHud\Addons\PointApiAddon
  * @depend PointAPI
  */
 namespace JackMD\ScoreHud\Addons
